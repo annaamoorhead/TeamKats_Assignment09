930caaa79416caa470acd056ea75781e9c8d1e4e
Owner <Owner@DESKTOP-P6S3L85>
2024-03-28 15:55:20 -0400

    first commit

---
+++ TeamKats_Assignment09/catClassPackage/catClass.py
@@ -0,0 +1,14 @@
+#catClass.py
+
+import requests
+import json
+
+    #response = requests.get('https://cat-fact.herokuapp.com/facts/random?animal_type=cat&amount=2')
+    #json_string = response.content
+    
+    #parsed_json = json.loads(json_string) # Now we have a python dictionary
+    
+    #status = int(parsed_json['status']) # The number of parks that were returned
+    
+    #for park in parsed_json['status']:
+        #print (park)
